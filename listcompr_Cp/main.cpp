d2e7241ce050fa9ccbb118e1ba5ff147d22e987d
Jesus Perez Colino <jpcolino@gmail.com>
2016-04-10 10:55:09 +0200

    Initial Commit

---
+++ listcompr_Cp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  listcompr_Cp
+//
+//  Created by Jesus Perez Colino on 10/04/16.
+//  Copyright © 2016 Jesus Perez Colino. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
